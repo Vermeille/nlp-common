a76bf0946e82cc123c545faf61bb116e8895e124
Guillaume "Vermeille" Sanchez <Guillaume.V.Sanchez@gmail.com>
2015-11-03 23:55:17 -0800

    initial

---
+++ document.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vector>
+
+typedef unsigned int Label;
+
+class LabelSet {
+  public:
+    Label GetLabel(const std::string& str) {
+        return labels_.insert(decltype (labels_)::value_type(str, labels_.size())).first->right;
+    }
+
+    void AddLabel(const std::string& str) { GetLabel(str); }
+
+    std::string GetString(Label pos) const {
+        return labels_.right.at(pos);
+    }
+
+    size_t size() const { return labels_.size(); }
+
+  private:
+    boost::bimap<std::string, Label> labels_;
+};
+
+struct WordFeatures {
+    size_t idx;
+    std::string str;
+};
+
+struct TrainingExample {
+    std::vector<WordFeatures> inputs;
+    Label output;
+};
+
+struct Document {
+    std::vector<TrainingExample> examples;
+};
+
