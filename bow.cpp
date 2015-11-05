f047346adea7aef01faa754e8cc193757fd8fd0d
Guillaume "Vermeille" Sanchez <Guillaume.V.Sanchez@gmail.com>
2015-11-05 03:54:44 -0800

    add a bag of words

---
+++ bow.cpp
@@ -0,0 +1,159 @@
+#include <glog/logging.h>
+
+#include "bow.h"
+
+static const unsigned int kNotFound = -1;
+static const double kLearningRate = 0.001;
+
+BagOfWords::BagOfWords(size_t in_sz, size_t out_sz)
+        : input_size_(in_sz),
+        output_size_(out_sz) {
+}
+
+void BagOfWords::Init() {
+    word_weight_.resize(output_size_);
+    for (int i = 0; i < output_size_; ++i) {
+        word_weight_[i].resize(input_size_);
+        for (int j = 0; j < input_size_; ++j) {
+            word_weight_[i][j] = 0;
+        }
+    }
+}
+
+double BagOfWords::WordF(Label target, const WordFeatures& w) const {
+    if (w.idx == kNotFound)
+        return 0;
+
+    return word_weight_[target][w.idx];
+}
+
+void BagOfWords::WordF_Backprop(const WordFeatures& w, Label truth, const double* probabilities) {
+    if (w.idx == kNotFound)
+        return;
+
+    for (int k = 0; k < output_size_; ++k) {
+        double target = (truth == k) ? 1 : 0;
+        word_weight_[k][w.idx] += kLearningRate * (target - probabilities[k]);
+    }
+}
+
+double BagOfWords::RunAllFeatures(Label k, const std::vector<WordFeatures>& ws) const {
+    double sum = 0;
+    for (int i = 0; i < ws.size(); ++i) {
+        sum += WordF(k, ws[i]);
+    }
+    return sum;
+}
+
+double BagOfWords::ComputeNLL(double* probas) const {
+    double nll = 0;
+    for (int i = 0; i < output_size_; ++i) {
+        nll += std::log(probas[i]);
+    }
+    return -nll;
+}
+
+Label BagOfWords::ComputeClass(const std::vector<WordFeatures>& ws, double* probabilities) const {
+    double total = 0;
+    for (int k = 0; k < output_size_; ++k) {
+        probabilities[k] = std::exp(RunAllFeatures(k, ws));
+        total += probabilities[k];
+    }
+
+    int max = 0;
+    for (int k = 0; k < output_size_; ++k) {
+        probabilities[k] /= total;
+        if (probabilities[k] > probabilities[max]) {
+            max = k;
+        }
+    }
+    return max;
+}
+
+void BagOfWords::Backprop(const std::vector<WordFeatures>& ws, Label truth, const double* probabilities) {
+    for (int i = 0; i < ws.size(); ++i) {
+        WordF_Backprop(ws[i], truth, probabilities);
+    }
+}
+
+int BagOfWords::Train(const Document& doc) {
+    double nll = 0;
+    double probas[output_size_];
+    int nb_correct = 0;
+    int nb_tokens = 0;
+
+    word_weight_.resize(output_size_);
+    for (int i = 0; i < output_size_; ++i) {
+        word_weight_[i].resize(input_size_);
+    }
+
+    for (auto& ex : doc.examples) {
+        Label predicted = ComputeClass(ex.inputs, probas);
+        nb_correct += predicted == ex.output ? 1 : 0;
+        ++nb_tokens;
+
+        nll += ComputeNLL(probas);
+
+        Backprop(ex.inputs, ex.output, probas);
+    }
+    return  nb_correct * 100 / nb_tokens;
+}
+
+std::string BagOfWords::Serialize() const {
+    std::ostringstream out;
+    out << input_size_ << " " << output_size_ << std::endl;
+
+    for (int w = 0; w < input_size_; ++w) {
+        for (int i = 0; i < output_size_; ++i) {
+            out << weight(i, w) << " ";
+        }
+        out << std::endl;
+    }
+    return out.str();
+}
+
+BagOfWords BagOfWords::FromSerialized(std::istream& in) {
+    std::string tok;
+    size_t in_sz, out_sz;
+    in >> in_sz >> out_sz;
+
+    BagOfWords bow(in_sz, out_sz);
+
+    for (int i = 0; i < bow.output_size_; ++i) {
+        bow.word_weight_.push_back({});
+    }
+
+    for (int w = 0; w < bow.input_size_; ++w) {
+        for (int i = 0; i < bow.output_size_; ++i) {
+            double score;
+            in >> score;
+            bow.word_weight_[i].push_back(score);
+        }
+    }
+
+    return bow;
+}
+
+void BagOfWords::ResizeInput(size_t in) {
+    if (in <= input_size_) {
+        return;
+    }
+
+    for (auto& weights : word_weight_) {
+        weights.resize(in);
+    }
+    input_size_ = in;
+}
+
+void BagOfWords::ResizeOutput(size_t out) {
+    if (out <= output_size_) {
+        return;
+    }
+
+    word_weight_.resize(out);
+    for (size_t i = output_size_; i < out; ++i) {
+        word_weight_[i].resize(input_size_);
+    }
+    output_size_ = out;
+}
+
---
+++ bow.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include <boost/bimap.hpp>
+
+#include "document.h"
+
+class BagOfWords {
+    std::vector<std::vector<double>> word_weight_;
+    size_t input_size_;
+    size_t output_size_;
+
+    void Init();
+
+    double WordF(Label target, const WordFeatures& w) const;
+    void WordF_Backprop(const WordFeatures& w, Label truth, const double* probabilities);
+    double RunAllFeatures(Label k, const std::vector<WordFeatures>& ws) const;
+    void Backprop(const std::vector<WordFeatures>& ws, Label truth, const double* probabilities);
+
+  public:
+    BagOfWords(size_t in_sz, size_t out_sz);
+    const std::vector<std::vector<double>>& weights() const { return word_weight_; }
+    const std::vector<double>& weights(size_t label) const { return word_weight_[label]; }
+    double weight(size_t label, size_t w) const { return word_weight_[label][w]; }
+
+    std::string Serialize() const;
+    static BagOfWords FromSerialized(std::istream& file);
+
+    double ComputeNLL(double* probas) const;
+    Label ComputeClass(const std::vector<WordFeatures>& ws, double* probabilities) const;
+
+    int Train(const Document& doc);
+
+    void ResizeInput(size_t in);
+    void ResizeOutput(size_t out);
+};
+
