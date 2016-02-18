25e5a3e547e0ad9a6e95223d5b69f6cd3ab676b9
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2016-02-18 02:26:22 +0100

    add new trainers

--- autodiff/src/ad/trainers.h
+++ autodiff/src/ad/trainers.h
@@ -9,14 +9,16 @@ template <class Updater>
 class FeedForwardTrainer {
     private:
         Updater updater_;
+
     public:
         FeedForwardTrainer(const Updater& updater)
             : updater_(updater) {
         }
 
-        template <class Model, class T, class U>
-        double Step(ComputationGraph& g, Model& model,
-                const T& input, const U& out) {
+        template <class ModelGen, class T, class U>
+        double Step(const T& input, const U& out, const ModelGen& model_gen) {
+            ComputationGraph g;
+            auto model = model_gen(g);
             Var h = model.Step(g, input);
             Var J = model.Cost(g, h, out);
 
@@ -24,6 +26,74 @@ class FeedForwardTrainer {
             g.Update(updater_);
 
             return J.value()(0, 0);
+        }
+};
+
+template <class Updater>
+class WholeSequenceTaggerTrainer {
+    private:
+        Updater updater_;
+
+    public:
+        WholeSequenceTaggerTrainer(const Updater& updater)
+            : updater_(updater) {
+        }
+
+        template <class ModelGen, class T, class U>
+        double Step(const T& input, const U& expected, const ModelGen& model_gen) {
+            if (input.empty()) {
+                return 0;
+            }
+
+            std::vector<Var> predicted;
+            ComputationGraph g;
+            auto model = model_gen(g);
+            for (auto in : input) {
+                predicted.push_back(model.Step(g, in));
+            }
+
+            Var J = model.Cost(g, predicted, expected);
+
+            g.BackpropFrom(J);
+            g.Update(updater_);
+
+            return J.value()(0, 0);
+        }
+};
+
+template <class Updater>
+class IterativeSequenceTaggerTrainer {
+    private:
+        Updater updater_;
+
+    public:
+        IterativeSequenceTaggerTrainer(const Updater& updater)
+            : updater_(updater) {
+        }
+
+        template <class ModelGen, class T, class U>
+        double Step(const T& input, const U& expected, const ModelGen& model_gen) {
+            if (input.empty()) {
+                return 0;
+            }
+
+            double nll = 0;
+            Var h(nullptr);
+            for (size_t last = 0; last < input.size(); ++last) {
+                ComputationGraph g;
+                auto model = model_gen(g);
+                for (size_t i = 0; i <= last; ++i) {
+                    h = model.Step(g, input[i]);
+                }
+
+                Var J = model.Cost(g, h, expected[last]);
+
+                nll += J.value()(0, 0);
+                g.BackpropFrom(J);
+                g.Update(updater_);
+            }
+
+            return nll / input.size();
         }
 };
 
